c92e1fc757e6e67ef9a9f0f7bb85885734ffb0ea
Peter <dchen@albany.edu>
2017-04-25 19:25:14 -0400

    Add the whole method cover all the function -Did not test though

--- Chapter 3 | Stacks and Queues/SetOfStacks.java
+++ Chapter 3 | Stacks and Queues/SetOfStacks.java
@@ -8,9 +8,7 @@
 // stack   (that is, pop() should return the same values as it would if there were 
 // just a single stack).
 
-// FOLLOW UP
-// Implement a function popAt(int index) which performs a pop operation on a
-// specic sub-stack.
+
 
 // In this problem, we’ve been told what our data structure should look like:
 
@@ -53,8 +51,10 @@ class Solution {
 
 		public Stack getLastStack() {
 			// to get the last element in the arrayList
-			Stack lastStack = stacks.get(stacks.size() - 1);
-			return lastStack;			
+			// Stack lastStack = stacks.get(stacks.size() - 1);
+			// return lastStack;		
+
+			// See the method down below	
 		}
 	}
 }
@@ -63,5 +63,152 @@ class Solution {
 to get the last element in the arrayList:
 	
       E e = list.get(list.size() - 1);
-      
+
 */
+
+// FOLLOW UP
+// Implement a function popAt(int index) which performs a pop operation on a
+// specic sub-stack.
+
+
+// This is a bit trickier to implement, but essentially we should imagine a  
+// “rollover” system. If we pop an element from stack 1, we need to remove
+//   the bottom of stack 2 and push it onto stack 1. We then need to
+// rollover from stack 3 to stack 2, stack 4 to stack 3, etc.
+
+// NOTE: You could make an argument that, rather than “rolling over,” we should 
+// be OK with some stacks not being at full capacity. This would improve  
+// the time complexity (by a fair amount, with a large number of
+// elements),   but it might get us into tricky situations later on if
+// someone   assumes that all stacks (other than the last) operate at full
+// capacity.   There’s no “right answer” here; discuss this trade-o  with
+// your   interviewer!
+
+class Solution {
+	public class SetOfStacks {
+		ArrayList<Stack> stacks = new ArrayList<Stack>();
+		public int capacity;
+		// constructor
+		public SetOfStacks(int capacity) {
+			this.capacity = capacity;
+		}
+
+		public Stack getLastStack() {
+			if (stacks.size() == 0) {
+				return null;
+			}
+			// last stack
+			return stacks.get(stacks.size() - 1);
+		}
+
+		public void push(int v) {
+			Stack lastStack = getLastStack();
+			if (lastStack != null && !lastStack.isAtCapacity()) {
+				lastStack.push(v);
+			} else { // create a new stack
+				Stack stack = new Stack(capacity);
+				stack.push(v);
+				stacks.add(stack);
+			}
+		}
+
+		public int pop() {
+			Stack lastStack = getLastStack();
+			// System.out.println(stacks.size());
+			int v = lastStack.pop();
+			// must do this step to make it work
+			if (lastStack.size() == 0) {
+				stacks.remove(stacks.size() - 1);
+			}
+			return v;
+		}
+
+		public int popAt(int index) {
+			return leftShift(index, true);
+		}
+
+		public int leftShift(int index, boolean removeTop) {
+			// get the current stack
+			Stack stack = stacks.get(index);
+			int removed_item;
+			if (removeTop) {
+				removed_item = stack.pop();
+			} else {
+				removed_item = stack.removeBottom();
+			}
+			// only one stack just remove the index one
+			if (stack.isEmpty()) {
+				stacks.remove(index);
+			} else if (stacks.size() > index + 1) {
+				int v = leftShift(index + 1, false);
+				stack.push(v);
+			}
+			return removed_item;
+		}
+	}
+	public class Stack {
+		private int capacity;
+		public Node top, bottom;
+		public int size = 0;
+
+		public Stack(int capacity) {
+			this.capacity = capacity;
+		}
+
+		public boolean isAtCapaity() {
+			return capacity == size;
+		}
+
+		public void join(Node above, Node below) {
+			if (below != null) {
+				below.above = above;
+				if (above != null) {
+					above.below = below;
+				}
+			}
+		}
+
+		public boolean push(int v) {
+			if (size >= capacity) {
+				return false;
+			}
+			size++;
+			Node n = new Node(v);
+			if (size == 1) {
+				bottom = n;
+			}
+			join(n, top);
+			top = n;
+			return true;
+
+		}
+
+		public int pop() {
+			Node t = top;
+			top = top.below;
+			size--;
+			return t.value;
+		}
+
+		public boolean isEmpty() {
+			return size == 0;
+		}
+
+		public int removeBotton() {
+			Node b = bottom;
+			bottom = bottom.above;
+			if (bottom != null) {
+				bottom.below = null;
+			}
+
+			size--;
+			return b.value;
+		}
+	}
+}
+
+/* 
+This whole thought is great , have to revisit this 
+and draw something out of this.
+*/
+    
