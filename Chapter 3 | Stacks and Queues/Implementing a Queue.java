bc6ab6129f80295e4e02755f3a90b91d277e0b3d
Peter <dchen@albany.edu>
2017-04-25 12:35:45 -0400

    Add a basic queue

---
+++ Chapter 3 | Stacks and Queues/Implementing a Queue.java
@@ -0,0 +1,22 @@
+// Implementing a Queue
+class Queue {
+	Node first, last;
+	void enqueue(Object item) {
+		if (!first) {
+			back = new Node(item);
+			first = back;
+		} else {
+			back.next = new Node(item);
+			back = back.next;
+		}
+	}
+
+	Node dequeue(Node n) {
+		if (front != null) {
+			Object item = front.data;
+			front = front.next;
+			return item;
+		}
+		return null;
+	}
+}
