d755c882bced39d78bcef6407fe8ef1bb361d540
Peter <dchen@albany.edu>
2017-05-08 14:01:58 -0400

    Add ava_primitive_types chart

---
+++ Chapter 5 | Bit Manipulation/5_1.java
@@ -0,0 +1,24 @@
+// You are given two 32-bit numbers, N and M, and two bit positions, i and j.
+// Write a method to set all bits between i and j in N equal to M   (e.g., M
+// becomes a   substring of N located at i and starting at j).
+// EXAMPLE:
+// Input: N = 10000000000, M = 10101, i = 2, j = 6
+// Output: N = 10001010100
+
+
+/*  This code operates by clearing all bits in N between position i and j, and 
+then ORing to put M in there.*/
+
+
+class Solution {
+    public static int updateBits(int n, int m, int i, int j) {
+        int max = ~0; /* All 1's */
+
+
+    }
+}
+
+/*
+1 Byte == 8bits
+
+*/
