958cdedfb0e18a7f173325c46c605603c4f4bb1f
Peter <dchen@albany.edu>
2017-05-08 18:14:32 -0400

    revised 5.1 add 5.6

---
+++ Chapter 5 | Bit Manipulation/5_6_swapBits.java
@@ -0,0 +1,9 @@
+// Write a program to swap odd and even bits in an integer with as few
+// instructions as possible (e.g., bit 0 and bit 1 are swapped, bit 2 and bit
+// 3 are swapped, etc).
+
+class Solution {
+    public static int swapOddEvenBits(int x) {
+        return (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
+    }
+}
