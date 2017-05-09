971ffa2b1d55f77fa9d25b923d52cfa5c96fca95
Peter <dchen@albany.edu>
2017-05-09 14:35:14 -0400

    Pouring Water

---
+++ Chapter 6 | Brain Teasers/Pouring Water.java
@@ -0,0 +1,13 @@
+/*
+    You have a  ve quart jug and a three quart jug, and an unlimited supply of water (but no measuring cups). How would you come up with exactly four quarts of water?
+    NOTE: The jugs are oddly shaped, such that  lling up exactly ‘half’ of the jug would be impossible.
+ */
+
+Action 1  pour  5(full)   pour into     3(empty)
+State  1        5(load 2)               3(load 3)
+Action 2  empty 5(load 2)               3(empty)
+Action 3  pour  5(load 2) pour into     3(empty)
+State  2        5(empty)                3(load 2)
+Action 4  load  5(full)                 3(load 2)
+Action 5  pour  5(full)   pour into     3(load 2)
+State  3        5(load 4)               3(load 3)     
